353500a56df48292208ef889e2d42ed8977c8459
Kenessino3 <MIJKennedy333@gmail.com>
2025-10-22 11:03:52 +0100

    test
    test

---
+++ Prototype_2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollisions : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        // Instead of destroying the projectile when it collides with an animal
+        //Destroy(other.gameObject); 
+
+        // Just deactivate the food and destroy the animal
+        other.gameObject.SetActive(false);
+        Destroy(gameObject);
+    }
+
+}
